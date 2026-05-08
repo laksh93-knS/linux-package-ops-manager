94616b5e3999980b584fdc8955fc02ad0283c8bd
laksh93-knS <lakshsonar93@gmail.com>
2026-05-08 12:06:16 +0530

    Update package_ops_manager.sh

--- package_ops_manager.sh
+++ package_ops_manager.sh
@@ -7,6 +7,7 @@ echo "Select an option:"
 echo "1. Update system"
 echo "2. Install package"
 echo "3. Remove package"
+echo "4. Check package installed or not"
 
 echo ""
 read -p "Enter your choice: " choice
@@ -24,6 +25,15 @@ elif [ "$choice" -eq 3 ]; then
     read -p "Enter package name to remove: " pkg
     sudo apt remove -y "$pkg"
     echo "Package removed: $pkg"
+
+elif [ "$choice" -eq 4 ]; then
+    read -p "Enter package name to check: " pkg
+
+    if rpm -q "$pkg" >/dev/null 2>&1; then
+        echo "Package is installed: $pkg"
+    else
+        echo "Package is not installed: $pkg"
+    fi
 
 else
     echo "Invalid option"
